c59ec611a346dab1720d8d7eed476abf614217df
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2021-12-29 20:35:20 -0600

    UpdateDesing

--- public/js/greeting.js
+++ public/js/greeting.js
@@ -2,13 +2,13 @@ function mostrarSaludo(){
     var texto = "";
     var ahora=new Date(); 
     var hora=ahora.getHours();
-    if(hora>=6 && hora<12){
+    if(hora>=1 && hora<12){
         texto="Buenos Días";  
     }
     if(hora>=12 && hora<21){ 
         texto="Buenas Tardes";
     }
-    if (hora>=21 || hora<6){ 
+    if (hora>=20 || hora<0){ 
         texto="Buenas Noches";
     }
    document.getElementById('greeting').innerHTML = texto;
